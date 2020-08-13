c10197bc1c8abafbc0386ade1621c37ed2653f33
Michael Carter <michael.carter@instaclustr.com>
2020-08-13 11:39:19 +1000

    fix inteeji being asmart

--- Algorithms/Special2/Michael-Main.java
+++ Algorithms/Special2/Michael-Main.java
@@ -2,8 +2,6 @@
 
 package com.hack;
 
-import basic.ShortScratch;
-
 import java.util.ArrayList;
 
 public class Main {
@@ -13,16 +11,16 @@ public class Main {
         bomberMan(9, new String[]{".......", "...O.O.", "....O..", "..O....", "OO...OO", "OO.O..."});
     }
 
-    static ArrayList<ShortScratch.State> pastState = new ArrayList<>();
+    static ArrayList<State> pastState = new ArrayList<>();
     public static String[] bomberMan(int n, String[] grid) {
         pastState.clear();
 
         for (int time = 0; time <= n; time++) {
-            ShortScratch.State bomb = null;
+            State bomb = null;
             if (time == 0) {
-                bomb = new ShortScratch.State(grid);
+                bomb = new State(grid);
             } else if (time == 1) {
-                bomb = new ShortScratch.State(pastState.get(0));
+                bomb = new State(pastState.get(0));
             } else if (time%2 == 0) {
                 bomb = pastState.get(time - 1).plant(time);
             } else if (time%2 == 1) {
@@ -44,13 +42,13 @@ public class Main {
         }
 
 
-        ShortScratch.State finalState = pastState.get(pastState.size() - 1);
+        State finalState = pastState.get(pastState.size() - 1);
         return finalState.toStringArray();
     }
 
     static void dump(int time) {
         System.out.println("--- "+time+" ---");
-        ShortScratch.State bomb = pastState.get(time);
+        State bomb = pastState.get(time);
         for (int i = 0; i < bomb.dimension1; i++) {
             for (int j = 0; j < bomb.dimension2; j++) {
                 if (bomb.slots[i][j].bombIsPresent) {
@@ -65,7 +63,7 @@ public class Main {
 
     static class State {
         public int dimension1, dimension2;
-        public ShortScratch.State.Slot[][] slots;
+        public State.Slot[][] slots;
 
         static class Slot {
             public boolean bombIsPresent;
@@ -75,10 +73,10 @@ public class Main {
         public State(String[] input) {
             dimension1 = input.length;
             dimension2 = input[0].length();
-            slots = new ShortScratch.State.Slot[dimension1][dimension2];
+            slots = new State.Slot[dimension1][dimension2];
             for (int i = 0; i < dimension1; i++) {
                 for (int j = 0; j < dimension2; j++) {
-                    ShortScratch.State.Slot slot = new ShortScratch.State.Slot();
+                    State.Slot slot = new State.Slot();
                     slot.bombIsPresent = input[i].charAt(j) == 'O';
                     slot.placementTime = 0;
                     slots[i][j] = slot;
@@ -86,13 +84,13 @@ public class Main {
             }
         }
 
-        public State(ShortScratch.State input) {
+        public State(State input) {
             dimension1 = input.dimension1;
             dimension2 = input.dimension2;
-            slots = new ShortScratch.State.Slot[dimension1][dimension2];
+            slots = new State.Slot[dimension1][dimension2];
             for (int i = 0; i < dimension1; i++) {
                 for (int j = 0; j < dimension2; j++) {
-                    ShortScratch.State.Slot slot = new ShortScratch.State.Slot();
+                    State.Slot slot = new State.Slot();
                     slot.bombIsPresent = input.slots[i][j].bombIsPresent;
                     slot.placementTime = input.slots[i][j].placementTime;
                     slots[i][j] = slot;
@@ -100,8 +98,8 @@ public class Main {
             }
         }
 
-        public ShortScratch.State plant(int time) {
-            ShortScratch.State newState = new ShortScratch.State(this);
+        public State plant(int time) {
+            State newState = new State(this);
             for (int i = 0; i < newState.dimension1; i++) {
                 for (int j = 0; j < newState.dimension2; j++) {
                     if (!newState.slots[i][j].bombIsPresent) {
@@ -113,8 +111,8 @@ public class Main {
             return newState;
         }
 
-        public ShortScratch.State explode(int time) {
-            ShortScratch.State newState = new ShortScratch.State(this);
+        public State explode(int time) {
+            State newState = new State(this);
             for (int i = 0; i < this.dimension1; i++) {
                 for (int j = 0; j < this.dimension2; j++) {
                     if (this.slots[i][j].bombIsPresent && this.slots[i][j].placementTime == time - 3) {
@@ -130,7 +128,7 @@ public class Main {
             return newState;
         }
 
-        public boolean sameAs(ShortScratch.State other, int otherTime, int time) {
+        public boolean sameAs(State other, int otherTime, int time) {
             for (int i = 0; i < dimension1; i++) {
                 for (int j = 0; j < this.dimension2; j++) {
                     if (other.slots[i][j].bombIsPresent != this.slots[i][j].bombIsPresent
