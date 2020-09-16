c82ffea171474971fb7431554b5d9b97200fee59
Subhani Munasinghe <subhani.munasinghe@instaclustr.com>
2020-09-17 02:49:40 +1000

    New

---
+++ Java/Eleven/src/com/company/Eleven.java
@@ -0,0 +1,35 @@
+//https://www.hackerrank.com/challenges/java-stack/problem
+
+package com.company;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Eleven {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            System.out.println(isBalanced(in.next()));
+        }
+        in.close();
+    }
+
+    static boolean isBalanced(String parentheses) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < parentheses.length(); i++) {
+            char ch = parentheses.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (stack.empty()) {
+                return false;
+            } else {
+                char top = stack.pop();
+                if ((top == '(' && ch != ')') || (top == '[' && ch != ']')
+                        || (top == '{' && ch != '}')) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
