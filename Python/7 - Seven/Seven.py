5dbdd70a6653509f00f2cdbe64fe47b537cd1b20
Subhani Munasinghe <subhani.munasinghe@instaclustr.com>
2020-11-11 22:44:54 +1100

    New

---
+++ Python/7 - Seven/Seven.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/re-sub-regex-substitution/problem
+
+import re
+
+def change(match):
+    if match.group(1) == '&&':
+        return 'and'
+    else:
+        return 'or'
+
+for _ in range(int(raw_input())):
+    print re.sub(r"(?<= )(\|\||&&)(?= )", change, raw_input())
