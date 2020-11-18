5609724ffcae52f3a9832f9250f58b72580ee660
Subhani Munasinghe <subhani.munasinghe@instaclustr.com>
2020-11-19 10:50:52 +1100

    Formatting a little bit

--- DataStructures/One.java
+++ DataStructures/One.java
@@ -8,16 +8,18 @@ The Node class is defined as follows:
      }
 */
 
-    boolean checkBST(Node root) {
-            return ValidateBST(root,Integer.MIN_VALUE, Integer.MAX_VALUE);
-            }
+    boolean checkBST(Node root){
+        return ValidateBST(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    }
 
-            boolean ValidateBST(Node root, int min, int max) {
-            if ((root != null) && (root.left.data < root.data && root.right.data > root.data)){
-//             condition satisfied
-        return ValidateBST(root.left,min, root.data-1) && ValidateBST(root.right,root.data+1,max);
+    boolean ValidateBST(Node root,int min,int max){
+
+        if((root!=null)&&(root.left.data<root.data&&root.right.data>root.data)){
+//      condition satisfied
+        return ValidateBST(root.left,min,root.data-1)&&ValidateBST(root.right,root.data+1,max);
+
         } else {
-        return false;
+
+            return false;
         }
-        }
-    
+    }
